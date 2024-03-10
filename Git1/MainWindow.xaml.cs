a19d0e6a493c956d52ac8d4e2932037f513b33a3
GrzegorzCygan <grzegorz.cygan.2023@gmail.com>
2024-03-10 11:50:24 +0100

    commit4

--- Git1/MainWindow.xaml.cs
+++ Git1/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace Git1
         public MainWindow()
         {
             InitializeComponent();
-            Console.WriteLine("commit3");
+            Console.WriteLine("commit4");
         }
     }
 }
